0f2e9c231890e63c182e94951995a2abe11e14ce
shubomnath88 <shubomnath88@gmail.com>
2018-06-13 21:51:39 +0530

    assignment2 done

---
+++ assignment2.py
@@ -0,0 +1,22 @@
+print("hello") #ques1
+#ques2
+a = input("name1")
+b = input("name2")
+print(a+" "+b)
+#ques3
+x = input("first=")
+y = input("second=")
+z = input("third=")
+print(x+" "+y+" "+z)
+#ques4
+print('let\'s get started')
+#ques5
+s = input()
+course = input()
+fees = input()
+print("institute is"+s+" course="+course+" fees="+str(fees))
+#ques6
+name = "tony"
+salary = 1000000
+print('%s''%d'%(name,salary))
+
